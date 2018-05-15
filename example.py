1469c33fd01d7a93d9f434b520e5d20360faa732
Dylan Kay <dylan.kay@me.com>
2018-05-15 11:33:32 +1000

    Example script

---
+++ example.py
@@ -0,0 +1,32 @@
+import requests
+import time
+import pprint
+
+SUICIDEWATCH = "https://api.pushshift.io/reddit/search/submission/?size=500&stickied=false&subreddit=suicidewatch&before={}"
+
+
+def write_to_file(out_file, body):
+    for row in body.split("\n"):
+        row = row.rstrip()
+        if len(row) > 0:
+            row = "__label__suicidal {}\n".format(row)
+            out_file.write(row)
+
+def suicidewatch(before=int(time.time())):
+    url = SUICIDEWATCH.format(before)
+
+    return requests.get(url).json()
+
+
+with open("data/suicidal.txt", "w") as out_file:
+    last = int(time.time())
+    while True:
+        res = suicidewatch(last)
+
+        for submission in res['data']:
+            try:
+                last = submission['created_utc']
+                write_to_file(out_file, submission['selftext'])
+                print("{} OK".format(submission['url']))
+            except Exception as ex:
+                print("{} FAIL".format(submission['url']))
